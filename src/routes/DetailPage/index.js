2e12f69cb7d2a1793a0ecd54041c913b1c6995d2
nullptru <nullptru@gmail.com>
2017-11-24 17:42:45 +0800

    detail style

--- src/routes/DetailPage/index.js
+++ src/routes/DetailPage/index.js
@@ -6,12 +6,12 @@ import styles from './index.less';
 const Detail = ({ article }) => {
   return (
     <div>
-      <article className="detail">
+      <article className={styles.detail}>
         <header>
           <div className={styles.title}>{ article.title }</div>
           <div className={styles.meta}>
-            <span className={styles.type}>{ article.type.label }</span>
             <span className={styles.time}>{ article.createdTime }</span>
+            <span className={styles.type}>{ article.type.label }</span>
           </div>
         </header>
         <section className={styles.content}>
@@ -23,9 +23,9 @@ const Detail = ({ article }) => {
             <a className={styles.tag} key={tag.value} href="#">{ tag.label }</a>) }
         </section>
       </article>
-      <div>
-        <Link to="">Previous Post</Link>
-        <Link to="">Next Post</Link>
+      <div className={styles.btnGroup}>
+        <Link to={article.pre || ''} className={styles.pre}><span>Previous Post</span></Link>
+        <Link to={article.next || ''} className={styles.next}><span>Next Post</span></Link>
       </div>
     </div>
   );
