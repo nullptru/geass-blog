f34351b759523239394d8a8498fec669f3effac0
nullptru <nullptru@gmail.com>
2017-11-27 20:15:20 +0800

    add more live2d

--- src/components/Live2D/index.js
+++ src/components/Live2D/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './indexLive2D';
+import { modelMorakumo, modelHaru, modelShizuku, modelMiku, modelHaruto } from './models';
 
 const PIXI = require('pixi.js');
 
 class Live2D extends React.PureComponent {
   componentDidMount() {
     /* eslint-disable */
-    const renderer = new PIXI.WebGLRenderer(300, 300, {transparent:true});
+    const renderer = new PIXI.WebGLRenderer(300, 350, {transparent:true});
     // renderer.backgroundColor = 0x00FFFFFF;
-    renderer.view.style.borderRadius = '300px';
+    // renderer.view.style.borderRadius = '300px';
     ReactDOM.findDOMNode(this).appendChild(renderer.view);
     const stage = new PIXI.Container();
-    const live2dSprite = new PIXI.Live2DSprite(this.modelHaru, {
+    const { model, config } = this.getModel();
+    const live2dSprite = new PIXI.Live2DSprite(model, {
       debugLog: false,
       randomMotion: false,
       eyeBlink: false,
@@ -23,9 +25,9 @@ class Live2D extends React.PureComponent {
     stage.addChild(live2dSprite);
 
     live2dSprite.x = -150;
-    // live2dSprite.y = -150;
-    live2dSprite.adjustScale(0.3,0.3,0.7);
-    live2dSprite.adjustTranslate(0, 0);
+    live2dSprite.y = -150;
+    live2dSprite.adjustScale(config.scaleX, config.scaleY, config.scaleZ);
+    live2dSprite.adjustTranslate(config.translateX, config.translateY);
     live2dSprite.startRandomMotion('idle');
 
     live2dSprite.on('click', (evt) => {
@@ -49,72 +51,11 @@ class Live2D extends React.PureComponent {
     animate();
   }
 
-  modelHaru = {
-    type: 'Live2D Model Setting',
-    name: 'haru',
-    model: '/haru/haru_01.moc',
-    textures:
-    [
-      '/haru/haru_01.1024/texture_00.png',
-      '/haru/haru_01.1024/texture_01.png',
-      '/haru/haru_01.1024/texture_02.png',
-    ],
-    physics: '/haru/haru.physics.json',
-    pose: '/haru/haru.pose.json',
-    expressions:
-    [
-      { name: 'f01', file: '/haru/expressions/f01.exp.json' },
-      { name: 'f02', file: '/haru/expressions/f02.exp.json' },
-      { name: 'f03', file: '/haru/expressions/f03.exp.json' },
-      { name: 'f04', file: '/haru/expressions/f04.exp.json' },
-      { name: 'f05', file: '/haru/expressions/f05.exp.json' },
-      { name: 'f06', file: '/haru/expressions/f06.exp.json' },
-      { name: 'f07', file: '/haru/expressions/f07.exp.json' },
-      { name: 'f08', file: '/haru/expressions/f08.exp.json' },
-    ],
-    layout:
-    {
-      center_x: 0,
-      y: 1.2,
-      width: 2.9,
-    },
-    hit_areas:
-    [
-      { name: 'head', id: 'D_REF.HEAD' },
-      { name: 'body', id: 'D_REF.BODY' },
-    ],
-    motions:
-    {
-      idle:
-      [
-        { file: '/haru/motions/idle_00.mtn', fade_in: 2000, fade_out: 2000 },
-        { file: '/haru/motions/idle_01.mtn', fade_in: 2000, fade_out: 2000 },
-        { file: '/haru/motions/idle_02.mtn', fade_in: 2000, fade_out: 2000 },
-      ],
-      tap_body:
-      [
-        { file: '/haru/motions/tapBody_00.mtn', sound: '/haru/sounds/tapBody_00.mp3' },
-        { file: '/haru/motions/tapBody_01.mtn', sound: '/haru/sounds/tapBody_01.mp3' },
-        { file: '/haru/motions/tapBody_02.mtn', sound: '/haru/sounds/tapBody_02.mp3' },
-      ],
-      pinch_in:
-      [
-        { file: '/haru/motions/pinchIn_00.mtn', sound: '/haru/sounds/pinchIn_00.mp3' },
-      ],
-      pinch_out:
-      [
-        { file: '/haru/motions/pinchOut_00.mtn', sound: '/haru/sounds/pinchOut_00.mp3' },
-      ],
-      shake:
-      [
-        { file: '/haru/motions/shake_00.mtn', sound: '/haru/sounds/shake_00.mp3', fade_in: 500 },
-      ],
-      flick_head:
-      [
-        { file: '/haru/motions/flickHead_00.mtn', sound: '/haru/sounds/flickHead_00.mp3' },
-      ],
-    },
-  };
+  getModel = () => {
+    const models = [ modelMorakumo, modelHaru, modelShizuku, modelMiku, modelHaruto ];
+    const date = new Date();
+    return models[date.getDay() % 5];
+  }
 
   render() {
     return <div id="live2d" />
