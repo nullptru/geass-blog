0a6bd20deba5b7b216ae7256c77715cbba6717bb
nullptru <nullptru@gmail.com>
2017-12-23 23:14:29 +0800

    bug fix && favicon && web title

--- src/routes/About/index.js
+++ src/routes/About/index.js
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import { HighLight } from 'components';
 
 export default () => {
-  const content = '> 人生は一度きりなんです、がっついていこう \n\n先给自己贴标签： 伪二次元、前端新手、拖延癌、日常咸鱼....\n\n虽然是个咸鱼却总想着搞事情，看了下时间，没错，还可以自称为应届大学生。\n\n博客的日常应该是主要分享一些技术上的想法和学习的记录，穿插一些日常生活的感悟和一些杂七杂八的东西。\n\n希望自己能够催促自己保持博客的活力，也欢迎交流各种问题～\n\n联系方式就先丢个QQ：1006755794';
+  const content = '> 人生は一度きりなんです、がっついていこう \n\n先给自己贴标签： 伪二次元、前端新手、拖延癌、日常咸鱼....\n\n虽然是个咸鱼却总想着搞事情，看了下时间，没错，还可以自称为应届大学生。\n\n博客的日常应该是主要分享一些技术上的想法和学习的记录，穿插一些日常生活的感悟和一些杂七杂八的东西。\n\n希望自己能够催促自己保持博客的活力，也欢迎交流各种问题～\n\n联系方式就先丢个QQ：1006755794\n\nGithub：[传送门](https://github.com/nullptru)';
   return (
     <div className="row" style={{ color: 'black', fontSize: '16px', margin: '24px auto', minHeight: '420px' } }>
       <div className="col-md-8 col-sm-12 col-md-push-2">
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'dva';
 import PropTypes from 'prop-types';
 import { Switch, Route } from 'dva/router';
+import DocumentTitle from 'react-document-title';
 import { Head, Footer, Live2D } from 'components';
 import './IndexPage.less';
 
@@ -11,6 +12,22 @@ class IndexPage extends React.PureComponent {
     return { routers, getRouteData };
   }
 
+  getPageTitle = (article) => {
+    let title = 'Geass Blog';
+    if (article.title) {
+      title = `${article.title}-Geass Blog`;
+    } else {
+      switch (this.props.location.pathname) {
+        case '/':
+          break;
+        case '/tagslist': title = 'Tags-Geass Blog'; break;
+        case '/about': title = 'About-Geass Blog'; break;
+        default:
+          break;
+      }
+    }
+    return title;
+  }
   render() {
     const { articles, getRouteData, location } = this.props;
     const headItems = [{
@@ -27,16 +44,18 @@ class IndexPage extends React.PureComponent {
     const { article } = articles;
     const [routers] = getRouteData('/');
     return (
-      <div id="app">
-        <Head items={headItems} article={article} location={location} />
-        <div className="container">
-          <Switch>
-            {routers.children.map(route => <Route key={route.path} path={route.path} component={route.component} exact={route.exact} />)}
-          </Switch>
+      <DocumentTitle title={this.getPageTitle(article)}>
+        <div id="app">
+          <Head items={headItems} article={article} location={location} />
+          <div className="container">
+            <Switch>
+              {routers.children.map(route => <Route key={route.path} path={route.path} component={route.component} exact={route.exact} />)}
+            </Switch>
+          </div>
+          <Live2D />
+          <Footer copyright="@CopyRight 2017 Blog of Geass" />
         </div>
-        <Live2D />
-        <Footer copyright="@CopyRight 2017 Blog of Geass" />
-      </div>
+      </DocumentTitle>
     );
   }
 }
