0eab8e3e33d0e6b828e2fc0ff5f472bfeabe55a9
nullptru <nullptru@gmail.com>
2018-01-17 22:21:09 +0800

    bug fix

--- blog-server/src/config.example.js
+++ blog-server/src/config.example.js
@@ -1,9 +1,19 @@
 const config = {
+  origin: 'http://localhost:8000',
   dbHost: 'dbHost',
   dbName: 'dbName',
-  dbUser: 'dbUser',
-  dbPwd: 'dbPwd',
+  dbUser: 'user',
+  dbPwd: 'yourPassword',
   appPort: 3000,
+  // qiniu
+  uploadConfig: {
+    scope: 'scope',
+    returnBody: '{ "key": "$(key)", "hash": "$(etag)", "w": "$(imageInfo.width)", "h": "$(imageInfo.height)", "name": "$(fname)"}',
+  },
+  uploadDir: './imgs',
+  defaultDomain: 'http://test.com/',
+  accessKey: 'key',
+  secretKey: 'key',
 };
 
 export default config;
